4cc191861e6e7684e841bc03f4c924076a512f8e
Steve Klabnik <steve@steveklabnik.com>
2014-10-23 09:05:00 -0400

    Hello, world.
    the app now boots up, it just doesn't really persist anything.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,5 +2,4 @@
 window.Todos = Ember.Application.create();
 
 Todos.ApplicationAdapter = DS.RESTAdapter.extend({
-	namespace: 'todos-emberjs'
 });
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 extern crate nickel;
+extern crate serialize;
 
 use std::io::net::ip::Ipv4Addr;
-use nickel::{Nickel, StaticFilesHandler};
+use nickel::{Nickel, Request, Response, HttpRouter, StaticFilesHandler};
+
+#[deriving(Decodable, Encodable)]
+struct Todo {
+    title: String,
+    is_completed: bool,
+}
 
 fn main() {
     let mut server = Nickel::new();
 
     server.utilize(StaticFilesHandler::new("frontend/"));
+
+    server.get("/todos", todos_handler);
     
     server.listen(Ipv4Addr(127, 0, 0, 1), 6767);
+}
+
+fn todos_handler (request: &Request, response: &mut Response) { 
+    response.send("{\"todos\":[]}"); 
 }
