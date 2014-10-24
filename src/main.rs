7574ea71d83e3bba0a1e35258ab2a30ce0b4ab74
Steve Klabnik <steve@steveklabnik.com>
2014-10-24 10:41:04 -0400

    clean up some style stuff

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,20 @@ extern crate http;
 
 use http::status;
 use std::io::net::ip::Ipv4Addr;
-use nickel::{ Nickel, Request, Response, HttpRouter, StaticFilesHandler, JsonBody, QueryString };
+use nickel::{
+    Nickel,
+    Request,
+    Response,
+    HttpRouter,
+    StaticFilesHandler,
+    JsonBody,
+    QueryString
+};
 
-use postgres::{ PostgresConnection, NoSsl };
+use postgres::{
+    PostgresConnection,
+    NoSsl
+};
 
 use std::collections::TreeMap;
 use serialize::json::ToJson;
@@ -39,18 +50,18 @@ fn main() {
     server.utilize(Nickel::json_body_parser());
     server.utilize(Nickel::query_string());
 
-    server.get("/todos", get_todos);
+    server.get( "/todos", get_todos);
     server.post("/todos", post_todo);
 
     server.listen(Ipv4Addr(127, 0, 0, 1), 6767);
 }
 
-fn get_todos (request: &Request, response: &mut Response) {
+fn get_todos(request: &Request, response: &mut Response) {
+    // this isn't super secure but it's also just a toy so whatever
     let conn = PostgresConnection::connect("postgres://rustmvc@localhost",
                                            &NoSsl).unwrap();
 
-    let stmt = conn.prepare("SELECT id, title, is_completed FROM todos")
-        .unwrap();
+    let stmt = conn.prepare("SELECT id, title, is_completed FROM todos").unwrap();
     let results = stmt.query([]).unwrap().map(|row| {
         Todo {
             id: row.get(0u),
@@ -66,7 +77,7 @@ fn get_todos (request: &Request, response: &mut Response) {
         .send(format!("{}", results));
 }
 
-fn post_todo (request: &Request, response: &mut Response) {
+fn post_todo(request: &Request, response: &mut Response) {
     let (status, body) = match request.json_as::<Todo>() {
         Some(t) => (http::status::Created, store_todo(t)),
         None => (http::status::BadRequest, "{\"error\":\"cannot be parsed\"}".to_string()),
@@ -78,7 +89,8 @@ fn post_todo (request: &Request, response: &mut Response) {
 fn store_todo(todo: Todo) -> String {
     let conn = PostgresConnection::connect("postgres://rustmvc@localhost", &NoSsl).unwrap();
 
-    conn.execute("INSERT INTO todos (title, is_completed) VALUES ($1, $2)", &[&todo.title, &todo.is_completed]).unwrap();
+    conn.execute("INSERT INTO todos (title, is_completed) VALUES ($1, $2)",
+                 &[&todo.title, &todo.is_completed]).unwrap();
 
     json::encode(&todo)
 }
